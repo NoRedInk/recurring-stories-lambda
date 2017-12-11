cb0fa4b35dc288fb78febcbc432aa6019880bc00
Marica Odagaki <marica@noredink.com>
2017-12-10 16:58:59 -0800

    Pull out mock event for testing in AWS console

--- tests.py
+++ tests.py
@@ -1,20 +1,9 @@
-import main
+import json
 
 import requests
 
+import main
 
-MOCK_EVENT = {
-    "account": "123456789012",
-    "region": "us-east-1",
-    "detail": {},
-    "detail-type": "Scheduled Event",
-    "source": "aws.events",
-    "time": "1970-01-01T00:00:00Z",
-    "id": "cdc73f9d-aea9-11e3-9d5a-835b769c0d9c",
-    "resources": [
-        "arn:aws:events:us-east-1:123456789012:rule/my-schedule"
-    ]
-}
 
 def test_process_event(mocker):
     mock_client = mocker.patch.object(main.boto3, 'client').return_value
@@ -25,9 +14,12 @@ def test_process_event(mocker):
     }
     mock_post = mocker.patch.object(requests, 'post')
     mock_move_to_top = mocker.patch.object(main, 'move_to_top_of_backlog')
-    config = main.read_config('config.json.sample')
 
-    rv = main.process_event(config, MOCK_EVENT)
+    config = main.read_config('config.json.sample')
+    with open('test_event.json', 'r') as f:
+        mock_event = json.load(f)
+
+    rv = main.process_event(config, mock_event)
 
     mock_post.assert_called()
     mock_move_to_top.assert_called()
